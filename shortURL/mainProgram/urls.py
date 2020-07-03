4c09a818b4050c31547fe57bec8c77ce97f6db61
Nikita <n.osypa666@gmail.com>
2020-07-03 21:56:36 +0300

    created view to return short url

--- shortURL/mainProgram/urls.py
+++ shortURL/mainProgram/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('',views.hello)
+    path('',views.mainPage,name='main_page')
 ]
--- shortURL/mainProgram/views.py
+++ shortURL/mainProgram/views.py
@@ -1,9 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render,reverse
 from django.http import HttpResponse
+from .models import StoreURL
 
 from .encoder import toBase62, fromBase62
 
-def hello(request):
-    print(toBase62(289))
-    return HttpResponse(fromBase62('F4'))
+def mainPage(request):
+    context = {}
+    if request.method == 'POST':
+        original_url = request.POST.get('original_url')
+        StoreURL.objects.create(url = original_url)
+        id = StoreURL.objects.latest('id').id
+        shortUrl = request.get_host() + '/' + toBase62(id)
+
+        context = {'shortUrl':shortUrl}
+
+    return render(request,'mainProgram/main.html',context)
 
